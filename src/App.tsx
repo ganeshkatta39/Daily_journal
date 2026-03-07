4880d160adb5e1c382f013c1e6fe4148244a0d7e
Ganesh <67380725+ganeshkatta39@users.noreply.github.com>
2026-03-07 21:31:19 +0530

    Revert "added: pitch detection test (#11)"
    This reverts commit 972a6a5f5258e7b9e9aedd8d1bb591b252e15fcf.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,10 @@ import PitchRecorder from "./components/PitchRecorder";
 import PitchUploadAnalyzer from "./components/PitchUploadAnalyzer";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
-import PitchTestHarness from "./components/PitchTestHarness";
 
 function App() {
 	return (
 		<>
-			{" "}
-			<PitchTestHarness />
 			<PitchRecorder />
 			<PitchUploadAnalyzer />
 			<Analytics />
